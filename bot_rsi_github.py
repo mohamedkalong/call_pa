1208a463e367569ab3bd0b3f61c8b36ee8cdbf15
mohamedkalong <mohamedkalong@gmail.com>
2026-01-20 17:45:18 +0700

    Update bot_rsi_github.py

--- bot_rsi_github.py
+++ bot_rsi_github.py
@@ -12,12 +12,12 @@ TELEGRAM_CHAT_ID = os.environ.get("TELE_CHATID")
 
 # --- CẤU HÌNH ĐIỀU KIỆN LỌC ---
 TIMEFRAME = '1h'
-LIMIT = 500            
+LIMIT = 244  #lấy bao nhiêu cây nến       
 RSI_PERIOD = 14
 
 # 3 ĐIỀU KIỆN CHÍNH:
 CHANGE_THRESHOLD = 15         # 1. Change > 20%
-VOLUME_THRESHOLD = 39_000_000 # 2. Volume 
+VOLUME_THRESHOLD = 55_000_000 # 2. Volume 
 RSI_THRESHOLD = 68            # 3. RSI > 70
 
 # ================= HÀM XỬ LÝ =================
